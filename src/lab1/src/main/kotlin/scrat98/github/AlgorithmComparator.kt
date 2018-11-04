19a47720efc6c8667aebea6e37d4a158c5f6a7b9
Artem Golovko <artem.golovko.ext@siemens.com>
2018-11-04 19:44:03 +0300

    add text statistic table

--- src/lab1/src/main/kotlin/scrat98/github/AlgorithmComparator.kt
+++ src/lab1/src/main/kotlin/scrat98/github/AlgorithmComparator.kt
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import javax.annotation.PostConstruct
 import kotlin.system.measureNanoTime
+import de.vandermeer.asciitable.AsciiTable
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment
+import java.io.File
 
 @SpringBootApplication
 class AlgorithmComparator(private val algorithms: Set<Algorithm>) : Runnable {
 
   companion object : KLogging()
 
-  private val statistics = mutableMapOf<String, MutableList<Statistic>>()
+  private val statistics = mutableMapOf<String, Pair<Double, MutableList<Statistic>>>()
 
   @PostConstruct
   override fun run() {
@@ -33,10 +36,46 @@ class AlgorithmComparator(private val algorithms: Set<Algorithm>) : Runnable {
             values = algorithm.values,
             time = time)
         statistics.computeIfAbsent(algorithm.toString()
-        ) { mutableListOf() }.add(statistic)
+        ) { algorithm.epsilon to mutableListOf() }.second.add(statistic)
       }
     }
-    logger.warn(statistics.toString())
+    printStatistics()
+  }
+
+  private fun printStatistics() {
+    logger.info("Writing statistics in files")
+    statistics.forEach { (algorithm, statistics) ->
+      val at = AsciiTable()
+      at.addRule()
+      at.addRow(
+          "stepNumber",
+          "initSection",
+          "newSection",
+          "reduction",
+          "points",
+          "values",
+          "time")
+      at.addRule()
+      statistics.second.forEach { stat ->
+        at.addRow(
+            "${stat.stepNumber}",
+            "${stat.initSection}",
+            "${stat.newSection}",
+            "${stat.reduction}",
+            "${stat.points}",
+            "${stat.values}",
+            "${stat.time}")
+            .setTextAlignment(TextAlignment.LEFT)
+        at.addRule()
+      }
+      val epsilon = statistics.first
+      val time: Long = statistics.second.fold<Statistic, Long>(0) { acc, it -> acc + it.time }
+      val steps = statistics.second.last().stepNumber
+      File("$algorithm.txt").printWriter().use { out ->
+        out.println("epsilon $epsilon steps $steps time $time ns")
+        out.println(at.render())
+      }
+    }
   }
 }
 
